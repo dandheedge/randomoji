bd66939bf77ad2b40b18b220cb7e8aca58b0fd9e
Dandhi <dandhi@biznetgio.com>
2022-07-29 14:44:01 +0700

    Edit: testing create new branch and component for bgn labs

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  name: "TestComponent",
+  components: {
+    SlideUpDown,
+  },
+};
+</script>
+<template>
+  <div class="flex flex-col mx-auto max-w-screen-md items-center py-8">
+    <h1 class="text-center text-amber-500">
+      Randomoji - Random Emoji Copypasta
+    </h1>
+</div
+</template>
+
+
+
